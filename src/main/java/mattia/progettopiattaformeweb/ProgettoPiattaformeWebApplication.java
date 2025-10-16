46ea84f66b2f5d92985bf00707f5cbea052948a2
mxttiapastore <pstmtt03d22d086v@studenti.unical.it>
2025-10-16 19:05:09 +0200

    Prima commit

---
+++ src/main/java/mattia/progettopiattaformeweb/ProgettoPiattaformeWebApplication.java
@@ -0,0 +1,13 @@
+package mattia.progettopiattaformeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoPiattaformeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoPiattaformeWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mattia/progettopiattaformeweb/ProgettoPiattaformeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package mattia.progettopiattaformeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoPiattaformeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
